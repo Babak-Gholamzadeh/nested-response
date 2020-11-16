85cf952da7e7c4471c7c378a9c0f6ecdf01a063a
Babak Gholamzadeh <babak.gholamzadeh92@gmail.com>
2020-11-16 23:33:07 +0330

    feat(nested-object): exclude undefined columns

--- helpers/make-nested-object.js
+++ helpers/make-nested-object.js
@@ -1,5 +1,6 @@
 const convertType = require('./convert-type');
 const deeplyObjectAssign = require('deeply-object-assign');
+const realTypeOf = require('realtypeof');
 
 const makeNestedObject = (data, def) => {
   const {
@@ -17,13 +18,22 @@ const makeNestedObject = (data, def) => {
         .entries(restColumns)
         .forEach(([defKey, defValue]) => {
           const { $column, $type, $default } = defValue;
+          // If the record value and default value are both undefined, skip that column
+          if (
+            !['array', 'object'].includes($type) &&
+            realTypeOf.isUndefined(record[$column]) &&
+            realTypeOf.isUndefined($default)
+          ) return;
+
           nestedObject[pk][defKey] = ['array', 'object'].includes($type) ?
             deeplyObjectAssign(
               nestedObject[pk][defKey] || {},
               makeNestedObject([record], defValue)
             ) :
             convertType(
-              record[$column] !== undefined ? record[$column] : $default
+              !realTypeOf.isUndefined(record[$column]) ?
+                record[$column] :
+                $default
             ).to($type);
         });
       return nestedObject;
